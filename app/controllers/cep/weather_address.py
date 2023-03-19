f786265fc0ad32125c3221274d0cd137aed7c96c
Matheus Feu <101346300+Havokzbr@users.noreply.github.com>
2023-03-19 18:43:47 -0300

    feat: informações da requisição

--- app/controllers/cep/weather_address.py
+++ app/controllers/cep/weather_address.py
@@ -1,4 +1,5 @@
 import xml.etree.ElementTree as ET
+from app import es_logger
 
 import requests
 import xmltodict
@@ -8,10 +9,19 @@ from unidecode import unidecode
 from app import app, require_keys
 from app.decorators.auth import token_required
 from app.decorators.regex_cep_validate import regex_cep_validate
+from app.utils.http_request_info import  HttpRequestInfo
 from config.urls_api import APICep, APIInpe
 
+http_request_info = HttpRequestInfo()
+ip_address = http_request_info.get_request_info('ip_address')
+user_agent = http_request_info.get_request_info('user_agent')
+geo_location = http_request_info.get_request_info('geo_location', ip_address)
+city_ip = geo_location['city'] if geo_location['city'] else 'Unknown'
+provider = geo_location['org'] if geo_location['org'] else 'Unknown'
 
-@app.route('/weather-address', methods=['GET'])
+log_format = f'IP: {ip_address} - User-Agent: {user_agent} - Provider: {provider} - City: {city_ip}'
+
+@app.route('/api/weather-address', methods=['GET'])
 @require_keys(['cep'])
 @regex_cep_validate
 @token_required()
@@ -20,6 +30,7 @@ def address_weather(current_user):
     e retorna a previsão do tempo em 4 dias"""
 
     cep = request.json['cep']
+
 
     # Faz a consulta do endereço pelo CEP na API ViaCep
     via_cep_response = requests.get(f"{APICep.URL}{cep}/json/")
@@ -30,8 +41,7 @@ def address_weather(current_user):
     city = via_cep_data_json.get('localidade')
 
     if not city:
-        return {'message': 'city not found'}, 404
-
+        return {'message': f'Cidade não encontrada: {city}'}, 404
     # Formata a cidade para consulta no INPE
     city_formated = unidecode(city)
 
@@ -43,10 +53,11 @@ def address_weather(current_user):
         response_inpe_xml = request_inpe.content
         response_inpe_dict = xmltodict.parse(response_inpe_xml)
         response_inpe_dict.update({"cep": via_cep_data_json})
+        es_logger.info(f'Consulta sucedida: {cep} - {log_format} - Codígo: {city_code}')
 
         return response_inpe_dict
-
-    return {'error': f'4 days time in city {city} not found in INPE API'}, 404
+    es_logger.warning(f'Não foi encontrado o clima para 4 dias na cidade {city} na API do INPE.')
+    return {'error': f'Não foi encontrado o clima para 4 dias na cidade {city} na API do INPE.'}, 404
 
 
 def generate_cod_inpe(city_formated, city):
