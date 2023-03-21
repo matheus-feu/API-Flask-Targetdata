d1270ddc84cf2dcebce96c0e6636d89deb630cc1
Matheus Feu <101346300+Havokzbr@users.noreply.github.com>
2023-03-21 16:28:35 -0300

    fix: descrição ajustada

--- app/config/swagger.py
+++ app/config/swagger.py
@@ -2,12 +2,8 @@ template = {
     "swagger": "2.0",
     "info": {
         "title": "Flask API - Targetdata - Consulta de CEP",
-        "description": "Esta API é capaz de fornecer aos usuários informações precisas sobre CEP, permitindo a consulta de dados relevantes como endereço, bairro, cidade e estado. "
-                       "Além disso, a API também oferece a funcionalidade de previsão do tempo para até quatro dias com base no CEP fornecido, permitindo que os usuários se planejem para as condições climáticas futuras."
-                       "\n\nPara garantir a segurança dos dados dos usuários, a API conta com um sistema de cadastro de usuários e login, que gera um token de acesso exclusivo para cada usuário. "
-                       "Esse token é necessário para acessar os endpoints da API e garantir que apenas usuários autorizados possam obter as informações disponíveis."
-                       "\n\nCom essa API, os usuários podem ter acesso a informações precisas e atualizadas sobre CEP e previsão do tempo, permitindo um planejamento mais eficiente e uma melhor organização das atividades diárias."
-        ,
+        "description": "API para consulta de CEP",
+
         "contact": {
             "email": "matheusfeu@gmail.com",
             "url": "https://www.linkedin.com/in/matheus-feu-558558186/",
