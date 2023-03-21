6e0bf080c2f5c12b7503af64615d7e36796891e7
Matheus Feu <101346300+Havokzbr@users.noreply.github.com>
2023-03-21 09:01:47 -0300

    feat: refatoração do __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,34 +1,36 @@
+from flasgger import Swagger
 from flask import Flask
 from flask_jwt_extended import JWTManager
 from flask_mongoengine import MongoEngine
 
-from app.utils.elasticsearch_logs import ElasticSearchLogger
-from config.model_elasticseach_log import LoggerConfigElasticsearch
-from flasgger import Swagger
+from app.config.swagger import swagger_config, template
 
+from app.models.elasticseach_log import LoggerConfigElasticsearch
+from app.utils.elasticsearch_logs import ElasticSearchLogger
+
+# Instanciando a aplicação
+app = Flask(__name__)
+
+# Configuração da aplicação
+app.config.from_object('config')
+
+# Configuração do banco de dados
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/test'
+db = MongoEngine(app)
+
+# Configuração da documentação Swagger
+swagger = Swagger(app, config=swagger_config, template=template)
+
+# Configuração do JWT
+jwt = JWTManager(app)
+
+# Instanciando a classe de logs
 es_logger = ElasticSearchLogger(
     index_name=LoggerConfigElasticsearch.index_name,
     elasticsearch_host=LoggerConfigElasticsearch.host)
 
 
-def create_app():
-    """Função responsável pela criação e instância do app Flask."""
-    app = Flask(__name__)
-    app.config.from_object('config')
-    app.config['MONGODB_SETTINGS'] = {
-        'db': 'test',
-        'host': 'localhost',
-        'port': 27017
-    }
-    swagger = Swagger(app, template_file='doc.yml')
-
-    jwt = JWTManager(app)
-    db = MongoEngine(app)
-
-    return app
-
-
-# Rotas do app
+# Rotas da aplicação
 from app.controllers.auth.login import *
 from app.controllers.auth.signup import *
 from app.controllers.cep.weather_address import *
