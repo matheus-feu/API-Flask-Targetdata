385ca2d3634467bb1225819b2880349c134af8c7
Matheus Feu <101346300+Havokzbr@users.noreply.github.com>
2023-03-21 16:27:59 -0300

    feat: configuração db

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,3 +9,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 SESSION_EXPIRATE_MINUTES = 10
 
 APP_NAME = 'Flask API Target Data - CEP'
+
+MONGODB_NAME = 'API_TARGET_DATA'
+MONGODB_HOST = 'localhost'
+MONGODB_PORT = 27017
+
