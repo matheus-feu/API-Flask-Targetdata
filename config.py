3b3c7aaf800735f4ac83112023d5316eebf0bb56
Matheus Feu <101346300+Havokzbr@users.noreply.github.com>
2023-03-29 12:24:29 -0300

    fix: configuração banco de dados

--- config.py
+++ config.py
@@ -7,11 +7,8 @@ load_dotenv()
 SECRET_KEY = os.environ.get('SECRET_KEY')
 SESSION_EXPIRATE_MINUTES = 10
 
-APP_NAME = 'Flask API Target Data - CEP'
-
 MONGODB_SETTINGS = {
     'db': "API_TARGET_DATA",
-    'host': os.environ.get('MONGODB_HOST', 'localhost'),
-    'port': int(os.environ.get('MONGODB_PORT', '27017'))
+    'host': os.environ.get('MONGODB_HOST'),
+    'port': int(os.environ.get('MONGODB_PORT'))
 }
-
